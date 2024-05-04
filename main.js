5f5f0244f7233ef47047e490b1d280244e12908c
Alessio Valastro <alessio@MacBook-Air-di-Alessio.local>
2024-05-04 16:16:20 +0200

    sistemati scrollTo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const toggler = document.getElementById("checkbox");
 const menu = document.getElementById("menu");
-const body = document.getElementById("body")
+const body = document.getElementById("body");
 
 toggler.addEventListener("click", () => {
   menu.classList.toggle("ject__nav-menu-toggle");
@@ -22,89 +22,91 @@ window.addEventListener("scroll", function () {
   lastScrollTop = scrollTop;
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  const menuLinks = document.querySelectorAll('#menu a');
-  menuLinks.forEach(function(link) {
-    link.addEventListener('click', function() {
-      document.getElementById('menu').classList.remove('ject__nav-menu-toggle');
-      body.classList.remove("body-block")
-      document.getElementById('checkbox').checked = false;
-
+document.addEventListener("DOMContentLoaded", function () {
+  const menuLinks = document.querySelectorAll("#menu a");
+  menuLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      document.getElementById("menu").classList.remove("ject__nav-menu-toggle");
+      body.classList.remove("body-block");
+      document.getElementById("checkbox").checked = false;
     });
   });
 });
 
-
-document.querySelectorAll('.faq__question').forEach(question => {
-  question.addEventListener('click', () => {
-    document.querySelectorAll('.careers__faq-question').forEach(faqBlock => {
-      faqBlock.classList.remove('active__faq');
+document.querySelectorAll(".faq__question").forEach((question) => {
+  question.addEventListener("click", () => {
+    document.querySelectorAll(".careers__faq-question").forEach((faqBlock) => {
+      faqBlock.classList.remove("active__faq");
     });
 
     const parent = question.parentNode;
     if (parent && parent.classList) {
-      parent.classList.add('active__faq');
+      parent.classList.add("active__faq");
     }
   });
 });
 
-let maxHeight = 0, maxWidth = 0;
+let maxHeight = 0,
+  maxWidth = 0;
 const values = document.querySelectorAll(".ject__more-content_values-value");
 
-values.forEach(value => {
+values.forEach((value) => {
   const height = value.offsetHeight;
   const width = value.offsetWidth;
   maxHeight = Math.max(maxHeight, height);
   maxWidth = Math.max(maxWidth, width);
 });
-values.forEach(value => {
+values.forEach((value) => {
   value.style.height = maxHeight + "px";
   value.style.width = maxWidth + "px";
-})
+});
 
-document.addEventListener("DOMContentLoaded", function() {
-  let heroBackground = document.querySelector('.background-image-hero');
+document.addEventListener("DOMContentLoaded", function () {
+  let heroBackground = document.querySelector(".background-image-hero");
   setupParallax(heroBackground);
 
-  let moreBackground = document.querySelector('.background-image-more');
+  let moreBackground = document.querySelector(".background-image-more");
   setupParallax(moreBackground);
 });
 
 function setupParallax(heroBackground) {
   function handleParallax() {
     let rect = heroBackground.getBoundingClientRect();
-    let translateY = (rect.top * 0.2);
-    heroBackground.style.transform = 'translateY(' + translateY + 'px)';
+    let translateY = rect.top * 0.2;
+    heroBackground.style.transform = "translateY(" + translateY + "px)";
   }
 
-  window.addEventListener('scroll', handleParallax);
+  window.addEventListener("scroll", handleParallax);
   handleParallax();
 }
 
-function ScrollToTop(){
+function ScrollToTop() {
   window.scrollTo({
-    top:0,
-    behavior: "smooth"
-  })
+    top: 0,
+    behavior: "smooth",
+  });
 }
 document.querySelector("#home").addEventListener("click", ScrollToTop);
 document.querySelector("#about").addEventListener("click", () => {
   document.querySelector(".ject__about").scrollIntoView({
-    behavior:"smooth"
-  })
-})
+    behavior: "smooth",
+  });
+});
 
 document.querySelector("#careers").addEventListener("click", () => {
   document.querySelector(".ject__careers").scrollIntoView({
-    behavior:"smooth"
-  })
-})
+    behavior: "smooth",
+  });
+});
+
+document.querySelector(".nav__button").addEventListener("click", () => {
+  document.querySelector(".ject__footer").scrollIntoView({
+    behavior: "smooth",
+  });
+});
 
 document.querySelector("#footer").addEventListener("click", () => {
-  console.log("ciao")
   document.querySelector(".ject__footer").scrollIntoView({
-    behavior:"smooth"
-  })
-})
-
-
+    behavior: "smooth",
+  });
+});
